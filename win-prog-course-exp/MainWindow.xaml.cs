a73e6b063676e452b20e8dde3c8fc5e79b717b7a
ZhengChengkai826 <zchengkai826@gmail.com>
2019-09-24 07:33:18 +0800

    improve

--- win-prog-course-exp/MainWindow.xaml.cs
+++ win-prog-course-exp/MainWindow.xaml.cs
@@ -26,23 +26,38 @@ namespace win_prog_course_exp
         public MainWindow()
         {
             InitializeComponent();
-            var root = new TreeViewItem() { Title = "Computer" };
-            root.Items.Add(new TreeViewItem() { Title = "HKEY_CLASSES_ROOT" });
-            root.Items.Add(new TreeViewItem() { Title = "HKEY_CURRENT_USER" });
-            root.Items.Add(new TreeViewItem() { Title = "HKEY_LOCAL_MACHINE" });
-            root.Items.Add(new TreeViewItem() { Title = "HKEY_USERS" });
-            root.Items.Add(new TreeViewItem() { Title = "HKEY_CURRENT_CONFIG" });
+            var root = new TreeViewItem(TreeViewItemType.COMPUTER) { Title = "Computer" };
+            root.Items.Add(new TreeViewItem(TreeViewItemType.FOLDER) { Title = "HKEY_CLASSES_ROOT" });
+            root.Items.Add(new TreeViewItem(TreeViewItemType.FOLDER) { Title = "HKEY_CURRENT_USER" });
+            root.Items.Add(new TreeViewItem(TreeViewItemType.FOLDER) { Title = "HKEY_LOCAL_MACHINE" });
+            root.Items.Add(new TreeViewItem(TreeViewItemType.FOLDER) { Title = "HKEY_USERS" });
+            root.Items.Add(new TreeViewItem(TreeViewItemType.FOLDER) { Title = "HKEY_CURRENT_CONFIG" });
             treeView.Items.Add(root);
         }
     }
 
+    public enum TreeViewItemType { COMPUTER, FOLDER, FOLDER_OPEN, }
+
     public class TreeViewItem
     {
-        public TreeViewItem()
+        public TreeViewItem(TreeViewItemType Type)
         {
             Items = new ObservableCollection<TreeViewItem>();
+            this.Type = Type;
 
-            Icon = new BitmapImage(new Uri("Resources/icon/computer.png", UriKind.Relative));
+            switch (Type)
+            {
+                case TreeViewItemType.COMPUTER:
+                    {
+                        Icon = new BitmapImage(new Uri("Resources/icon/computer.png", UriKind.Relative));
+                        break;
+                    }
+                case TreeViewItemType.FOLDER:
+                    {
+                        Icon = new BitmapImage(new Uri("Resources/icon/folder.png", UriKind.Relative));
+                        break;
+                    }
+            }
         }
 
         public ObservableCollection<TreeViewItem> Items { get; set; }
@@ -50,6 +65,8 @@ namespace win_prog_course_exp
         public string Title { get; set; }
 
         public ImageSource Icon { get; set; }
+
+        public TreeViewItemType Type { get; set; }
 
     }
 }
