4babadb56be0f0fa4a1665908cd6e6796220a7d4
ZhengChengkai826 <zchengkai826@gmail.com>
2019-09-23 23:23:23 +0800

    beautify & data binding

--- win-prog-course-exp/MainWindow.xaml.cs
+++ win-prog-course-exp/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Interop;
 using System.Runtime.InteropServices;
+using System.Collections.ObjectModel;
 
 namespace win_prog_course_exp
 {
@@ -25,6 +26,30 @@ namespace win_prog_course_exp
         public MainWindow()
         {
             InitializeComponent();
+            var root = new TreeViewItem() { Title = "Computer" };
+            root.Items.Add(new TreeViewItem() { Title = "HKEY_CLASSES_ROOT" });
+            root.Items.Add(new TreeViewItem() { Title = "HKEY_CURRENT_USER" });
+            root.Items.Add(new TreeViewItem() { Title = "HKEY_LOCAL_MACHINE" });
+            root.Items.Add(new TreeViewItem() { Title = "HKEY_USERS" });
+            root.Items.Add(new TreeViewItem() { Title = "HKEY_CURRENT_CONFIG" });
+            treeView.Items.Add(root);
         }
+    }
+
+    public class TreeViewItem
+    {
+        public TreeViewItem()
+        {
+            Items = new ObservableCollection<TreeViewItem>();
+
+            Icon = new BitmapImage(new Uri("Resources/icon/computer.png", UriKind.Relative));
+        }
+
+        public ObservableCollection<TreeViewItem> Items { get; set; }
+
+        public string Title { get; set; }
+
+        public ImageSource Icon { get; set; }
+
     }
 }
