816e2fa8a9d79f95ceebaca15b9b0cb5fcf3ad28
ZhengChengkai826 <zchengkai826@gmail.com>
2019-10-01 20:35:10 +0800

    ref

---
+++ filezckmanaged/Class1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace filezckmanaged
+{
+    public enum FileTreeViewItemType { FILE, FOLDER, FOLDER_OPEN, }
+
+    public class FileTreeViewItem : INotifyPropertyChanged
+    {
+        public FileTreeViewItem(string Path)
+        {
+            Items = new ObservableCollection<FileTreeViewItem>();
+            this.Path = Path;
+            var fileInfo = new FileInfo(Path);
+            Title = fileInfo.Name;
+            if (fileInfo.Attributes.HasFlag(FileAttributes.Directory))
+            {
+                Type = FileTreeViewItemType.FOLDER;
+            }
+            else
+            {
+                Type = FileTreeViewItemType.FILE;
+            }
+            FileQueried = false;
+        }
+        public ObservableCollection<FileTreeViewItem> Items { get; set; }
+        public string Title { get; set; }
+        private FileTreeViewItemType type;
+        public FileTreeViewItemType Type
+        {
+            get { return type; }
+            set
+            {
+                type = value;
+                OnPropertyChanged("Type");
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged == null)
+            {
+                return;
+            }
+            PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+        public string Path { get; set; }
+        public bool FileQueried { get; set; }
+        public FileTreeViewItem Parent { get; set; }
+        public void queryFile()
+        {
+            if (Type == FileTreeViewItemType.FILE || FileQueried)
+            {
+                return;
+            }
+
+            Items.Clear();
+            var subDirs = Directory.EnumerateDirectories(Path);
+            foreach (var d in subDirs)
+            {
+                Items.Add(new FileTreeViewItem(d) { Parent = this });
+            }
+            var files = Directory.EnumerateFiles(Path);
+            foreach (var f in files)
+            {
+                Items.Add(new FileTreeViewItem(f) { Parent = this });
+            }
+
+            FileQueried = true;
+        }
+    }
+}
--- win-prog-course-exp/MainWindow.xaml.cs
+++ win-prog-course-exp/MainWindow.xaml.cs
@@ -17,6 +17,7 @@ using System.Runtime.InteropServices;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.IO;
+using filezckmanaged;
 
 namespace win_prog_course_exp
 {
@@ -200,7 +201,7 @@ namespace win_prog_course_exp
             var dlg = new NewKeyDlg() { PromptText = "Please enter the folder name:" };
             if (dlg.ShowDialog() == true)
             {
-                var context = FileTree.SelectedItem as FileTreeViewItem;
+                var context = FileTree.SelectedItem as  FileTreeViewItem;
                 if(context.Type == FileTreeViewItemType.FILE)
                 {
                     context = context.Parent;
@@ -444,72 +445,6 @@ namespace win_prog_course_exp
 
                 hKeyQueried = true;
             }
-        }
-    }
-
-    public enum FileTreeViewItemType { FILE, FOLDER, FOLDER_OPEN, }
-
-    public class FileTreeViewItem : INotifyPropertyChanged
-    {
-        public FileTreeViewItem(string Path)
-        {
-            Items = new ObservableCollection<FileTreeViewItem>();
-            this.Path = Path;
-            var fileInfo = new FileInfo(Path);
-            Title = fileInfo.Name;
-            if(fileInfo.Attributes.HasFlag(FileAttributes.Directory))
-            {
-                Type = FileTreeViewItemType.FOLDER;
-            } else
-            {
-                Type = FileTreeViewItemType.FILE;
-            }
-            FileQueried = false;
-        }
-        public ObservableCollection<FileTreeViewItem> Items { get; set; }
-        public string Title { get; set; }
-        private FileTreeViewItemType type;
-        public FileTreeViewItemType Type
-        {
-            get { return type; }
-            set
-            {
-                type = value;
-                OnPropertyChanged("Type");
-            }
-        }
-        public event PropertyChangedEventHandler PropertyChanged;
-        public void OnPropertyChanged(string propertyName)
-        {
-            if (PropertyChanged == null)
-            {
-                return;
-            }
-            PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
-        }
-        public string Path { get; set; }
-        public bool FileQueried { get; set; }
-        public FileTreeViewItem Parent { get; set; }
-        public void queryFile()
-        {
-            if (Type == FileTreeViewItemType.FILE || FileQueried)
-            {
-                return;
-            }
-
-            Items.Clear();
-            var subDirs = Directory.EnumerateDirectories(Path);
-            foreach(var d in subDirs)
-            {
-                Items.Add(new FileTreeViewItem(d) { Parent = this });
-            }
-            var files = Directory.EnumerateFiles(Path);
-            foreach(var f in files)
-            {
-                Items.Add(new FileTreeViewItem(f) { Parent = this });
-            }
-
-            FileQueried = true;
         }
     }
 }
