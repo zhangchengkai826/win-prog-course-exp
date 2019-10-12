215e492605360162e5fcdd3dcefbd76ec18a2a9e
ZhengChengkai826 <zchengkai826@gmail.com>
2019-10-12 14:01:13 +0800

    remove signing

--- win-prog-course-exp/ChapterContent3.xaml.cs
+++ win-prog-course-exp/ChapterContent3.xaml.cs
@@ -21,6 +21,8 @@ namespace win_prog_course_exp
     /// </summary>
     public partial class ChapterContent3 : UserControl
     {
+        private string mac;
+        private int getMacOutputLineId;
         public ChapterContent3()
         {
             InitializeComponent();
@@ -28,26 +30,36 @@ namespace win_prog_course_exp
 
         private void GetMac(object sender, RoutedEventArgs e)
         {
+            mac = "";
+            getMacOutputLineId = 0;
             Process process = new Process();
             process.StartInfo.FileName = "cmd.exe";
-            // 是否使用外壳程序   
+            // 不使用外壳程序
             process.StartInfo.UseShellExecute = false;
-            // 是否在新窗口中启动该进程的值   
+            // 不在新窗口中启动该进程
             process.StartInfo.CreateNoWindow = true;
-            // 重定向输入流  
+            // 重定向输入流
             process.StartInfo.RedirectStandardInput = true;
             // 重定向输出流
             process.StartInfo.RedirectStandardOutput = true;
-            //使ping命令执行九次 
+            process.OutputDataReceived += new DataReceivedEventHandler(GetMacOutputHandler);
             string strCmd = "getmac /FO table /NH";
             process.Start();
+            process.BeginOutputReadLine();
             process.StandardInput.WriteLine(strCmd);
             process.StandardInput.WriteLine("exit");
-            // 获取输出信息   
-            var output = process.StandardOutput.ReadToEnd();
-            Mac.Text = output;
             process.WaitForExit();
             process.Close();
+            Mac.Text = "网卡mac为： " + mac;
+        }
+
+        private void GetMacOutputHandler(object sendingProcess, DataReceivedEventArgs outLine)
+        {
+            if(getMacOutputLineId == 5)
+            {
+                mac = outLine.Data.Split()[0];
+            }
+            getMacOutputLineId++;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
