07dccfb15a9fc2d1654dc8f1e1fe996fa94015c1
ZhengChengkai826 <zchengkai826@gmail.com>
2019-10-15 14:04:47 +0800

    add

---
+++ win-prog-course-exp/ChapterContent5.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace win_prog_course_exp
+{
+    /// <summary>
+    /// Interaction logic for ChapterContent5.xaml
+    /// </summary>
+    public partial class ChapterContent5 : UserControl
+    {
+        public ChapterContent5()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
